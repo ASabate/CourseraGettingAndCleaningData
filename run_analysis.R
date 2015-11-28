ce3c24dbcd54a7ea6bfd37f2e9798b024aa4163b
ASabate <ASabate@users.noreply.github.com>
2015-11-28 16:53:46 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -3,44 +3,34 @@
 # Date: 22/11/2015
 ####################################################################
 
-library(reshape2)
 # Load activity labels
-activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
-activityLabels[,2] <- as.character(activityLabels[,2])
+activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
 # Load features labels
 features <- read.table("UCI HAR Dataset/features.txt")
-features[,2] <- as.character(features[,2])
+# search mean and Std measures
+featmeanStdPos<-grep(".*mean.*|.*std.*", features$V2);
 
-# Extract only the measurements on the mean and standard deviation
-featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
-featuresWanted.names <- features[featuresWanted,2]
-featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names)
-featuresWanted.names = gsub('-std', 'Std', featuresWanted.names)
-featuresWanted.names <- gsub('[-()]', '', featuresWanted.names)
+xtrain<-read.table("UCI HAR Dataset/train/X_train.txt")
+xtrainmeanstd<-xtrain[featmeanStdPos]
+ytrain<-read.table("UCI HAR Dataset/train/Y_train.txt")
+subjecttrain<-read.table("UCI HAR Dataset/train/subject_train.txt")
 
+trainjoin<-cbind(subjecttrain, ytrain,xtrainmeanstd)
 
-# Load training data
-train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresWanted]
-trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
-trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
-train <- cbind(trainSubjects, trainActivities, train)
+xtest<-read.table("UCI HAR Dataset/test/X_test.txt")
+xtestmeanstd<-xtest[featmeanStdPos]
+ytest<-read.table("UCI HAR Dataset/test/Y_test.txt")
+subjecttest<-read.table("UCI HAR Dataset/test/subject_test.txt")
+testjoin<-cbind(subjecttest, ytest, xtestmeanstd)
 
-# Load test data
-test <- read.table("UCI HAR Dataset/test/X_test.txt")[featuresWanted]
-testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
-testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
-test <- cbind(testSubjects, testActivities, test)
+traintestjoin<-rbind(trainjoin, testjoin)
 
-# Merge the training and the test sets to create one data set
-allData <- rbind(train, test)
-colnames(allData) <- c("subject", "activity", featuresWanted.names)
-allData$activity <- factor(allData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
-allData$subject <- as.factor(allData$subject)
-allData.melted <- melt(allData, id = c("subject", "activity"))
-allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
+colnames(traintestjoin)<-c("subject","activity",as.character(features$V2[featmeanStdPos]))
+traintestjoin$activity<-factor(traintestjoin$activity, levels=activity_labels$V1,labels=activity_labels$V2)
+traintestjoin$subject<-as.factor(traintestjoin$subject)
 
 # Output result into a text file
-write.table(allData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
+write.table(traintestjoin, "tidy.txt", row.names = FALSE, quote = FALSE)
 
 
 
